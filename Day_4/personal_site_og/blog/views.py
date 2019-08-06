be2b9e52986470a22896a76652c5dcd044a334ab
Beau Schelble <bschelb@clemson.edu>
2019-08-06 19:01:43 -0400

    Bootstrap part 1

---
+++ Day_4/personal_site_og/blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from blog.models import Post
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.list import ListView
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ['title', 'body']
+    success_url = '/blog/list'
+
+class PostList(ListView):
+    model = Post
+
+class PostUpdate(UpdateView):
+    model = Post
+    fields = ['title', 'body']
+    success_url = '/blog/list'
+
+class PostDelete(DeleteView):
+    model = Post
+    success_url = '/blog/list'
